0adb6588cc6c4630ead031514c2cdc7ceced2acf
firaflash <flexflash09@gmail.com>
2025-11-07 23:55:46 +0300

    removed the codes for the dbs managment from frontend

--- public/artShop/script.js
+++ public/artShop/script.js
@@ -179,7 +179,7 @@ function hideLoader() {
   document.getElementById("loader").style.display = "none";
 }
 async function fetchProducts() {
-  const response = await fetch("http://localhost:5555/api/dbs/fetchProduct", {
+  const response = await fetch("/api/dbs/fetchProduct", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
