7cab338dd8f71ba04f9485753b6da9f45a589c91
firaflash <flexflash09@gmail.com>
2025-11-10 12:29:25 +0300

    Changed the extension of chapacontrollers to an ES Module

--- api/chapaRoutes.js
+++ api/chapaRoutes.js
@@ -1,8 +1,5 @@
 import { Router } from 'express';
-import chapaController from './chapacontroller.cjs';
-// If `chapaController.cjs` is a CommonJS module, import it as the default and
-// destructure the exported functions so Node's ESM loader resolves them.
-const { proceedPayment, verifyPayment } = chapaController;
+import { proceedPayment, verifyPayment } from './chapacontroller.js';
 
 const router = Router();
 
