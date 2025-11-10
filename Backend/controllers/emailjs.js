6a912f38cdf9fbac302e7cd9dfd11e347d318828
firaflash <flexflash09@gmail.com>
2025-11-11 00:23:39 +0300

    Last Touches

--- Backend/controllers/emailjs.js
+++ Backend/controllers/emailjs.js
@@ -1,10 +1,4 @@
-const emailServiceID = process.env.EMAILJS_SERVICE_ID;
-const emailTemplateID = process.env.EMAILJS_TEMPLATE_ID;
-const emailUserID = process.env.EMAILJS_PRIVATE_KEY;
-
-// utils/sendEmail.js
-export const sendConfirmationEmail = async (templateParams) => {
-  console.log(emailServiceID + " " + emailTemplateID + " " + emailUserID);
+export const sendConfirmationEmail = async (templateParams) => {  
   const payload = {
     service_id: process.env.EMAILJS_SERVICE_ID,
     template_id: process.env.EMAILJS_TEMPLATE_ID,
--- FrontEnd/Home/artShop/config/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-const supabaseUrl = 'https://euxhwztkmhzyrcwoupne.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1eGh3enRrbWh6eXJjd291cG5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MTQ1MTMsImV4cCI6MjA2NzE5MDUxM30.87GhA7C5b9RVsp49EnwZBq-LRDsbviEPSsE1bFjkYOM';
-// Use your actual anon/public API key
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
